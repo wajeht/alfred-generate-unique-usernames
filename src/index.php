f77d24dbc69739ff53ee1a6f5f62eb985a0006db
wajeht <58354193+wajeht@users.noreply.github.com>
2022-11-19 22:06:21 -0500

    chore: removed animals

--- src/index.php
+++ src/index.php
@@ -1,6 +1,13 @@
 <?php
 
 $adjectives = json_decode(file_get_contents(__DIR__ . '/data/adjectives.json'));
-$animals = json_decode(file_get_contents(__DIR__ . '/data/animals.json'));
 $nouns = json_decode(file_get_contents(__DIR__ . '/data/nouns.json'));
 $verbs = json_decode(file_get_contents(__DIR__ . '/data/verbs.json'));
+
+$adjectives = $adjectives[array_rand($adjectives, 1)];
+$nouns = $nouns[array_rand($nouns, 1)];
+$verbs = $verbs[array_rand($verbs, 1)];
+
+$username = $adjectives . $nouns . $verbs;
+
+echo $username;
